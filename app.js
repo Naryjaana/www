86a3b667260664b136d29aafb8c278c6ffaa53d8
Naryjaana Ivanova <uana99790@gmail.com>
2023-05-07 20:34:33 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,104 @@
+// 1 Переменные
+//const isprogrammer = true
+//const firstName = 'vLAD'
+//const LastName = 'gjgf'
+
+
+// 2 мутирование
+//prompt ('ввелите имя' )
+//alert (firstName + ' ' + LastName)
+
+
+//3 оператор
+
+//const currentYear = 2023
+//const birthYear = 2004
+
+//const age = currentYear - birthYear
+
+//console.log(age)
+//console.log(typeof currentYear)
+
+
+//6 условные операторы
+//const courseStatus = 'fail' //ready, pending, fail
+
+//if (courseStatus === 'ready') {
+  //  console.log('курс уже готов и его можно проходить')
+//} else if (courseStatus === 'pending') {
+   // console.log('курс пока находится в процессе разработки')
+//} else {
+   // console.log('курс не получился')
+//}
+
+// == true, === лучше применять false
+
+
+//тернарное выражение
+//isReady? console.log('готово') : console.log('не готово')
+
+
+//булевая логика
+//true && true = true, true || true = true 
+//false && true = false, false || true = true
+//false && false = false,  false || false = false
+
+
+// 8  функция
+
+//function calculateAge(year) {
+  // return 2023 - year
+//}
+
+//const myAge = calculateAge(2004)
+//console.log(myAge)
+
+//function loginfoabout( name1, year) {
+    //const age = calculateAge(year)
+
+    //console.log(' человек по имени ' + name1 + ' сейчас имеет возраст ' + age)
+//}
+
+//loginfoabout( "Владислав",   2004 )
+
+// массивы
+
+//const cars = ['мазда', 'мерседес', 'форд']
+//console.log(cars) правильней всего
+//const cars =  new Array('мазда', 'мерседес', 'форд')
+//console.log(cars) более подробно
+
+//console.log(cars[1])
+//console.log(cars.length)
+
+//циклы
+//const cars = ['мазда', 'мерседес', 'форд']
+
+//for (let i = 0; i < cars.length; i++) {
+//console.log(i)
+//const car = cars[i]
+//console.log(car)
+//}
+
+//for (let car of cars) {
+   //console.log(car)
+//} Более современное
+
+
+//объект
+//const person = new Object({})
+//const person = {
+  // firstName: 'Vlad',
+  // lastName: 'minion',
+   //year: 1993,
+   //languages: ['ru', 'eng'],
+   //hasWife: false,
+   //greet: function() {
+    // console.log('greet from person') 
+ //  }
+//}
+
+//console.log(person.firstName)
+//const key = 'languages'
+//console.log(person[key])
+//person.greet()
---
+++ index.js
@@ -0,0 +1,9 @@
+let a;
+let b;
+let c;
+
+a=1;
+b=5;
+c=a*b;
+
+console.log("c", c);
